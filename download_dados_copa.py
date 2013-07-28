1140ae4028ad02a3640434e6f9ae9e7cb7a60ace
Felipe cruz <felipecruz@loogica.net>
2013-07-28 19:47:48 -0300

    Download files with no content-length in header
    response. 
    Url is a user input now.

--- download_dados_copa.py
+++ download_dados_copa.py
@@ -2,6 +2,7 @@
 
 # import simples
 import io
+import sys
 
 #import com mudança de namespace
 import urllib.request as request
@@ -10,8 +11,15 @@ import urllib.request as request
 # String
 # Constante
 # Junção com \
-MES_09_12_URL = 'http://www.portaldatransparencia.gov.br/copa2014/gestor/' \
-                'download?nomeArquivo=201209_BaseDados.zip'
+HEADER_URL = 'http://www.portaldatransparencia.gov.br/copa2014/gestor/' \
+             'download?nomeArquivo=201209_BaseDados.zip'
+
+NO_LENGTH_HEADER = 'http://www.portaldatransparencia.gov.br/copa2014/gestor/' \
+                   'download?nomeArquivo=20130711_BaseDados.zip'
+
+#http://www.portaldatransparencia.gov.br/copa2014/gestor/download?nomeArquivo=201209_BaseDados.zip
+#http://www.portaldatransparencia.gov.br/copa2014/gestor/download?nomeArquivo=20130711_BaseDados.zip
+
 
 # Inteiro
 # Constante
@@ -24,25 +32,32 @@ def main():
     # Identação
 
     # Chamada de função
-    response = request.urlopen(MES_09_12_URL)
-
-    # Conversão de string para inteiro
-    length = int(response.getheader('Content-Length'))
-
-    # Conversão de float para inteiro
-    times = int((length / BUFF_SIZE) + 1)
-
-    # Imprime variáveis locais
-    print(locals())
+    response = request.urlopen(sys.argv[1])
 
     # I/O usando io.FileIO
     out_file = io.FileIO("saida.zip", mode="w")
 
-    # Loop de 0 até "times"
-    for time in range(times):
-        # Escreve em "out_file" o que ler em "response"
-        out_file.write(response.read(BUFF_SIZE))
-        print("Downloaded %d" % (((time * BUFF_SIZE)/length)*100))
+    content_length = response.getheader('Content-Length')
+    if content_length:
+        # Conversão de string para inteiro
+        length = int(content_length)
+        # Conversão de float para inteiro
+        times = int((length / BUFF_SIZE) + 1)
+        # Loop de 0 até "times"
+        for time in range(times):
+            # Escreve em "out_file" o que ler em "response"
+            out_file.write(response.read(BUFF_SIZE))
+            print("Downloaded %d" % (((time * BUFF_SIZE)/length)*100))
+    else:
+        total_downloaded = 0
+        while True:
+            # Escreve em "out_file" o que ler em "response"
+            data = response.read(BUFF_SIZE)
+            total_downloaded += len(data)
+            if not data:
+                break
+            out_file.write(data)
+            print('Downloaded {bytes}'.format(bytes=total_downloaded))
 
     # Fecha Response
     response.close()
