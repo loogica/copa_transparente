b9af3a9ae417c045b1aa9586a4abd231b30b53c9
Felipe cruz <felipecruz@loogica.net>
2014-04-11 20:47:11 -0300

    Suporte ao relatório de cobertura de testes

--- runtests.py
+++ runtests.py
@@ -4,6 +4,21 @@ import sys
 
 from copa_transparente.tests import JSONTestRunner
 
+try:
+    import coverage
+except:
+    coverage = None
+
 if __name__ == "__main__":
+    if coverage:
+        cov = coverage.coverage(branch=True, source=['copa_transparente'])
+        cov.start()
+
     loader = unittest.TestLoader()
     JSONTestRunner().run(loader.discover('copa_transparente/tests/'))
+
+    if coverage:
+        cov.stop()
+        cov.save()
+        cov.html_report(directory='htmlcov')
+        cov.report(show_missing=False)
