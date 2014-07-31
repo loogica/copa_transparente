e4565586ddb06499e99495ed55d4eda25d29b94e
Felipe cruz <felipecruz@loogica.net>
2014-07-31 01:25:30 -0300

    Unifica leitor de meta dados no script principal

--- copa_transparente/commands/download_dados_copa.py
+++ copa_transparente/commands/download_dados_copa.py
@@ -4,10 +4,14 @@ import datetime
 import io
 import logging
 import os
+import pickle
 import sys
 import zipfile
 
 import urllib.request as request
+
+
+from copa_transparente import domain, utils
 
 
 BUFF_SIZE = 1024
@@ -81,12 +85,58 @@ def extract_zip(file_name, path="banco"):
 
     os.chdir(current_path)
 
-def main():
-    url = sys.argv[1]
-    timestamp = datetime.datetime.now().strftime("%Y%m%d")
-    download_url(url, "data_{}.zip".format(timestamp))
-    extract_zip("data_{}.zip".format(timestamp))
+
+def read_meta_data():
+    meta = {}
+    for meta_data in os.listdir("banco/meta-data"):
+        meta_file = os.path.join("banco/meta-data", meta_data)
+        meta_name = meta_data.replace(".txt", "")
+        table_timestamp, table_name = utils.extract_table_name(meta_name)
+        text_file = open(meta_file, "r", encoding="utf-8")
+        data = text_file.read()
+        columns = data.split('\n')
+        meta[table_name] = domain.DataTable(table_name)
+        read_lines(meta, table_name, columns)
+
+    pks = {}
+
+    for table_name, data_table in meta.items():
+        pk = data_table.pk
+        columns = data_table.cols[2:]
+        pks[pk.name] = data_table
+        for rel_table, rel_meta in meta.items():
+            if rel_table != table_name:
+                rel_pk = rel_meta.pk
+                rel_columns = rel_meta.cols[2:]
+                for column in rel_columns:
+                    if column == pk:
+                        data_table.add_referenced(column.name, rel_meta, column)
+
+    for table_name, data_table in meta.items():
+        columns = data_table.cols
+        for column in columns:
+            if column.name in pks:
+                data_table.add_references(column.name, pks[column.name], column)
 
 
-if __name__ == "__main__":
-    main()
+    out_file = open('banco/datamodel.pickle', 'wb')
+    out_file.write(pickle.dumps(meta))
+    out_file.close()
+
+
+def read_lines(meta, table_name, columns):
+    for i, column in enumerate(columns):
+        if column:
+            values = column.split('\t')
+            nome = values[0]
+            tipo = values[1]
+            desc = values[2]
+            try:
+                if i == 0:
+                    meta[table_name].add_pk(nome, tipo, desc)
+                else:
+                    meta[table_name].add_column(nome, tipo, desc)
+            except:
+                print("Erro com Tabela {} Coluna {} tipo {}".format(table_name,
+                                                                    nome,
+                                                                    tipo))
